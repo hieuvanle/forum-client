fa53df95b60ca0269dcb84aa77996febb23a3d10
Hieu Van Le <hieule20901@gmail.com>
2020-05-25 09:00:13 -0500

    Updates

---
+++ precache-manifest.ce58b21253268268f7b7a226a41d1ffc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6886c6aa80661fb8c2531e9cbc3b6cd3",
+    "url": "/forum-client/index.html"
+  },
+  {
+    "revision": "e4dcac187981f4a249ab",
+    "url": "/forum-client/static/js/2.1d1e6b7c.chunk.js"
+  },
+  {
+    "revision": "8a9a2ae4a25f202f52818f8f8074424e",
+    "url": "/forum-client/static/js/2.1d1e6b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f91ca90876b15d030545",
+    "url": "/forum-client/static/js/main.b3869e48.chunk.js"
+  },
+  {
+    "revision": "12b06b1b7bfe937d35fa",
+    "url": "/forum-client/static/js/runtime-main.6f0e2675.js"
+  }
+]);
