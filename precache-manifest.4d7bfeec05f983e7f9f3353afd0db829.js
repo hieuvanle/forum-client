db575c744129878cd1062a40df6b8d881fa6d9ed
Hieu Van Le <hieule20901@gmail.com>
2020-05-26 20:38:19 -0500

    Updates

--- precache-manifest.4d7bfeec05f983e7f9f3353afd0db829.js
+++ precache-manifest.4d7bfeec05f983e7f9f3353afd0db829.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a462b829561e97354b6ee4338287645d",
+    "revision": "811d98ebec9eed7f2f200d9cc1ab0544",
     "url": "/forum-client/index.html"
   },
   {
-    "revision": "f1dc57c811b41f7ca7de",
-    "url": "/forum-client/static/js/2.bf241e24.chunk.js"
+    "revision": "57048cb332f3c5964940",
+    "url": "/forum-client/static/js/2.bd2b7423.chunk.js"
   },
   {
     "revision": "8a9a2ae4a25f202f52818f8f8074424e",
-    "url": "/forum-client/static/js/2.bf241e24.chunk.js.LICENSE.txt"
+    "url": "/forum-client/static/js/2.bd2b7423.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "57b5d4c38dd124027128",
-    "url": "/forum-client/static/js/main.b673f0d3.chunk.js"
+    "revision": "96161bcad321ea57e363",
+    "url": "/forum-client/static/js/main.0ee93794.chunk.js"
   },
   {
     "revision": "12b06b1b7bfe937d35fa",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/forum-client/precache-manifest.a71d4fdfb8c549b373d1ef7cd6863c2a.js"
+  "/forum-client/precache-manifest.4d7bfeec05f983e7f9f3353afd0db829.js"
 );
 
 self.addEventListener('message', (event) => {
